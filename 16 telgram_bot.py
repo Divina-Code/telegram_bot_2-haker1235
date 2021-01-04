af9459ba8ded883200d5a1a6fd8f9ced73adc246
haker1235 <grebenkin.ilya07@gmail.com>
2021-01-04 17:45:15 +0300

    123

--- 16 telgram_bot.py
+++ 16 telgram_bot.py
@@ -1,19 +1,17 @@
 import telebot
-
-
-bot = telebot.TeleBot('your token')  #Создаём подключение к боту
+from myToken import TOKEN
+bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(content_types=['text'])
 def otvet_na_text(message):
-    print(message)
-    bot.send_message(message.chat.id, message.text[::-1])
-
-
-
-
-
-bot.polling() #Забираем сообщения у бота
-
-
-
+    mes = message.text
+    user = message.chat.username
+    id = message.chat.id
+    inp = message.text.lower().split()
+    for word in inp:
+        if word[::-1] == word:
+            bot.send_message(id, f"{word} это палиндром")
+        else:
+            bot.send_message(id, f"{word} это не палиндром")
+bot.polling()
