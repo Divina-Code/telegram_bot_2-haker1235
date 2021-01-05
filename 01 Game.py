7e72ccb406ee7dd593b8230a8b7916cfa0ac7a57
haker1235 <grebenkin.ilya07@gmail.com>
2021-01-05 20:17:03 +0300

    Update 01 Game.py

--- 01 Game.py
+++ 01 Game.py
@@ -8,7 +8,7 @@ gamedisplay = pygame.display.set_mode((800, 600) )
 gamedisplay.fill((255, 255, 255))
 
 radius = 5
-color = (0, 0, 0)
+color = (255, 0, 0)
 game = True  #Продолжается игра или нет
 while game:
 
@@ -26,7 +26,7 @@ while game:
             if e.button == 5:
                 radius -= 1
 
-            if e.button == pygame.BUTTON_RIGHT:
+            if e.button == 2:
                 color = (randint(0, 255), randint(0, 255), randint(0, 255))
 
 
@@ -34,6 +34,9 @@ while game:
         mousepose = pygame.mouse.get_pos()
         pygame.draw.circle(gamedisplay, color, mousepose, radius)
 
+    if pygame.mouse.get_pressed()[2]:
+        mousepose = pygame.mouse.get_pos()
+        pygame.draw.circle(gamedisplay, (255, 255, 255), mousepose, radius)
 
     pygame.display.update()
 pygame.quit()
