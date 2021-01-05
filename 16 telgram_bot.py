c27d89b9eb4d2fd2580b821f3a72b721bc843c9d
haker1235 <grebenkin.ilya07@gmail.com>
2021-01-05 14:19:42 +0300

    Update 16 telgram_bot.py

--- 16 telgram_bot.py
+++ 16 telgram_bot.py
@@ -1,12 +1,34 @@
-import telebot
+
 from myToken import TOKEN
-from random import randint
+import telebot
+from random import choice, shuffle
+
+words = ['антарктида', 'амплитуда', 'локомотив', 'индустриализация']
+
+word = choice(words).lower()
+shuffle_word = list(word)
+shuffle(shuffle_word)
+a = 0
+
+
 bot = telebot.TeleBot(TOKEN)
+@bot.message_handler(content_types = ['text'])
 
-
-@bot.message_handler(content_types=['text'])
 def otvet_na_text(message):
+    global a
+    if a == 0:
+        bot.send_message(message.chat.id,"Угадай слово: "+ ''.join(shuffle_word) +'\t')
+        a = a + 1
+        message.text.lower().split()
 
-    random_number = randint(1000, 9999)
-    bot.send_message(message.chat.id, "Ваш пароль - ", random_number)
+    else:
+        if message.text == word:
+            bot.send_message(message.chat.id,"Угадал")
+            bot.send_message(message.chat.id,"#"*30)
+            a = 0
+
+        else:
+            bot.send_message(message.chat.id,"Не Угадал")
+
+
 bot.polling()
