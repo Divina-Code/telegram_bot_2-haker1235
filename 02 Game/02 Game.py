ecbe2a9fc6c0523ed43041abbaf4037604f34ac8
haker1235 <grebenkin.ilya07@gmail.com>
2021-01-07 15:06:20 +0300

    Update 02 Game.py

--- 02 Game/02 Game.py
+++ 02 Game/02 Game.py
@@ -16,31 +16,32 @@ if answer1 == '1':
 
     while life:
 
-        if answer2 == 1:
+        if answer2 == '1':
             myFile2 = open("1 room.txt")
             print(myFile2.read())
             myFile2.close()
             life = 0
             print("Вы проиграли")
-        if answer2 == 2:
+        elif answer2 == '2':
             myFile3 = open("2 room.txt")
             print(myFile3.read())
             myFile3.close()
             items = 1
             print("У вас есть один предмет, вам нужен второй")
-        if answer2 == 3:
+            life = 1
+        elif answer2 == '3':
             myFile4 = open("3 room.txt")
             print(myFile4.read())
             myFile4.close()
             life = 0
             print("вы проиграли")
-        if answer2 == 4:
+        elif answer2 == '4':
             myFile5 = open("4 room.txt")
             print(myFile5.read())
             myFile5.close()
             life = 0
             print("Вы проиграли")
-        if answer2 == 5:
+        elif answer2 == '5':
             myFile6 = open("5 room.txt")
             print(myFile6.read())
             myFile6.close()
