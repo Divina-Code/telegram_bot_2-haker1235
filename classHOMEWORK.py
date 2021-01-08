64a7f37c3decefa59f6f8d950e2dabae765dab00
haker1235 <grebenkin.ilya07@gmail.com>
2021-01-08 14:15:53 +0300

    Update classHOMEWORK.py

--- classHOMEWORK.py
+++ classHOMEWORK.py
@@ -1,4 +1,4 @@
-from random import randint, choice
+from random import randint
 
 
 class Warrior():
@@ -20,7 +20,37 @@ class Warrior():
 
 karl = Warrior("Karl", 100, randint(5, 15), True)
 gerakl = Warrior("Gerakl", 100, randint(5, 15), True)
-print(karl)
-print(gerakl)
+print(karl.name, karl.hp, karl.damage, karl.life)
+print(gerakl.name, gerakl.hp, gerakl.damage, gerakl.life)
 
+while karl.life and gerakl.life:
 
+    # сражение
+    karl.take_damage(gerakl.damage)
+    print("Karl hp =", karl.hp)
+    gerakl.take_damage(karl.damage)
+    print("Geracl hp =", gerakl.hp)
+
+    # плюс хп за покушать
+    karl.hp += randint(2, 5)
+    print("Karl hp =", karl.hp)
+
+    gerakl.hp += randint(2, 5)
+    print("Geracl hp =", gerakl.hp)
+
+    #баф урона
+
+    karl.damage += randint(2, 5)
+    print("Karl damage =", karl.damage)
+
+    gerakl.damage += randint(2, 5)
+    print("Karl damage =", karl.damage)
+
+    #проверка кто проиграл
+    if karl.hp <= 0:
+        karl.life = False
+        print("Karl lost")
+
+    if gerakl.hp <= 0:
+        gerakl.life = False
+        print("Geracl lost")
