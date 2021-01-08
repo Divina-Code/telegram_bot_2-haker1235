7b4908ceeb5d57a2a0cd1aa84de62bd99a7697da
haker1235 <grebenkin.ilya07@gmail.com>
2021-01-08 17:03:57 +0300

    1

--- classHOMEWORK.py
+++ classHOMEWORK.py
@@ -18,39 +18,50 @@ class Warrior():
         self.damage += lvl
 
 
+class Archer(Warrior):
+    def __init__(self, name, hp, damage, life, bow):
+        super().__init__(name, hp, damage, life)
+        self.bow = bow
+
+    def shoot(self):
+        print("shooting")
+
+
+snaiper = Archer("Snaiper", 100, randint(5, 15), True, 56)
 karl = Warrior("Karl", 100, randint(5, 15), True)
 gerakl = Warrior("Gerakl", 100, randint(5, 15), True)
-print(karl.name, karl.hp, karl.damage, karl.life)
-print(gerakl.name, gerakl.hp, gerakl.damage, gerakl.life)
 
-while karl.life and gerakl.life:
+print(karl.name, "hp =", karl.hp, "damage =", karl.damage, "life =", karl.life)
+print(gerakl.name, "hp =", gerakl.hp, "damage =", gerakl.damage, "life =", gerakl.life)
+print(snaiper.name, "hp =", snaiper.hp, "damage =", snaiper.damage, "life =", snaiper.life, "bows =", snaiper.bow)
+# while karl.life and gerakl.life:
 
-    # сражение
-    karl.take_damage(gerakl.damage)
-    print("Karl hp =", karl.hp)
-    gerakl.take_damage(karl.damage)
-    print("Geracl hp =", gerakl.hp)
+# сражение
+# karl.take_damage(gerakl.damage)
+# print("Karl hp =", karl.hp)
+# gerakl.take_damage(karl.damage)
+# print("Geracl hp =", gerakl.hp)
 
-    # плюс хп за покушать
-    karl.hp += randint(2, 5)
-    print("Karl hp =", karl.hp)
+# плюс хп за покушать
+# karl.hp += randint(2, 5)
+# print("Karl hp =", karl.hp)
 
-    gerakl.hp += randint(2, 5)
-    print("Geracl hp =", gerakl.hp)
+# gerakl.hp += randint(2, 5)
+# print("Geracl hp =", gerakl.hp)
 
-    #баф урона
+# баф урона
 
-    karl.damage += randint(2, 5)
-    print("Karl damage =", karl.damage)
+# karl.damage += randint(2, 5)
+# print("Karl damage =", karl.damage)
 
-    gerakl.damage += randint(2, 5)
-    print("Karl damage =", karl.damage)
+# gerakl.damage += randint(2, 5)
+# print("Karl damage =", karl.damage)
 
-    #проверка кто проиграл
-    if karl.hp <= 0:
-        karl.life = False
-        print("Karl lost")
+# проверка кто проиграл
+# if karl.hp <= 0:
+# karl.life = False
+# print("Karl lost")
 
-    if gerakl.hp <= 0:
-        gerakl.life = False
-        print("Geracl lost")
+# if gerakl.hp <= 0:
+# gerakl.life = False
+# print("Geracl lost")
--- draw chess.py
+++ draw chess.py
@@ -1,1 +1,29 @@
+import pygame
 
+gamedisplay = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Snowmans")
+
+
+def draw_chess(x, y, startX = 100, startY = 100, size = 30):
+
+    pygame.draw.rect(gamedisplay, BROWN, (nowx ,now), 25 )
+
+
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+BROWN = (150, 75, 0)
+RED = (255, 0, 0)
+
+game = True
+while game:
+
+    for e in pygame.event.get():
+
+        if e.type == pygame.QUIT:
+            game = False
+
+
+    draw_chess(8,8)
+
+    pygame.display.update()
+pygame.quit()
